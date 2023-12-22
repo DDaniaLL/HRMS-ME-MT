5f43eee0b3b3c38435558aa96a780b84cb8e8f24
DDaniall <dany.janboura@gmail.com>
2023-12-22 23:16:23 +0300

    fixed a little bit the system settings leave type index page table

--- resources/views/admin/leavetypes/index.blade.php
+++ resources/views/admin/leavetypes/index.blade.php
@@ -20,24 +20,28 @@
                           @php
                           $user = Auth::user()
                       @endphp
-                      @if ($user->office == "CO-Erbil")
-                      @if ($user->hradmin == 'yes')
+            
                              <div   div class="col-12 text-right">
                                 <a href="{{route('admin.leavetypes.create')}}" class="btn btn-sm btn-primary">{{__('systemoffices.addOffice')}}</a>
                              </div>
-                      @endif
-                      @endif
+                  
                         </div>
-                        <div class="card-body table-responsive-md">
+                        <div class="card-body table-responsive">
 
-                          <div class="row">
-                        <table class="table table-hover text-nowrap table-Secondary">
+                         
+                          <table id="table_iddd" class="table w-100 table-responsive table-bordered table-hover text-nowrap table-Secondary table-striped">
                         <thead>
-                            <tr>
-                              <th class="text-center" scope="col">{{__('systemoffices.id')}}</th>
-                              <th class="text-center" scope="col">{{__('systemoffices.name')}}</th>
-                              <th class="text-center" scope="col">{{__('systemoffices.value')}}</th>
-                              
+                          <tr style=" background-color: #ffb678 !important;">
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.id')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.name')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.value')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.active')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.accrualtype')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.national')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.international')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.service')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.custom')}}</th>
+                              <th class="text-center" style="width: 10%" scope="col">{{__('systemoffices.order')}}</th>
                              
                             </tr>
                           </thead>
@@ -47,13 +51,19 @@
                               <td class="text-center">{{ $leavetype->id }}</td>
                               <td class="text-center">{{ $leavetype->name }}</td>
                               <td class="text-center">{{ $leavetype->value }}</td>
-                              
+                              <td class="text-center">{{ $leavetype->active }}</td>
+                              <td class="text-center">{{ $leavetype->accrualtype }}</td>
+                              <td class="text-center">{{ $leavetype->national }}</td>
+                              <td class="text-center">{{ $leavetype->international }}</td>
+                              <td class="text-center">{{ $leavetype->service }}</td>
+                              <td class="text-center">{{ $leavetype->custom }}</td>
+                              <td class="text-center">{{ $leavetype->order }}</td>
                             </tr>
                             @endforeach
                           </tbody>
                       </table>
 
-                          </div>
+                        
 
                         </div>
                       </div>
@@ -61,55 +71,23 @@
 
 
 
-                      @foreach ($leavetypes as $leavetype)
+                     
 
-
-                      <div id="myModal{{$leavetype->id}}" class="modal fade" role="dialog">
-                          <div class="modal-dialog modal-sm">
-
-                            <!-- Modal content-->
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h4 style="color: red" class="modal-title">Attention!</h4>
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                              </div>
-                              <div class="modal-body">
-                                <p>Are you sure you want to delete: <br><strong>{{$leavetype->name}}</strong>.</p>
-                                <form method="POST" action="{{ route('admin.leavetypes.destroy', $leavetype) }}" class="text-center" >
-                                  {{ csrf_field() }}
-                                  {{ method_field('DELETE') }}
-                                  <div class="form-group">
-                                      <input type="submit" class="btn btn-danger" value="Delete">
-                                  </div>
-                              </form>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                              </div>
-                            </div>
-
-                          </div>
-                        </div>
-                        @endforeach
-
-                        <style>
-                            table td {
-                              font-size: 20px;
-                            }
-                           </style>
                   </div>
               </div>
           </div>
  @endsection
  @push('scripts')
 
+ <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
  <script>
+  $('#table_iddd').DataTable(
+      {
+          "order": [[ 0, "asc" ]],
+          
+      }
+  );
 
-    var myModal = document.getElementById('myModal')
-    var myInput = document.getElementById('myInput')
-
-    myModal.addEventListener('shown.bs.modal', function () {
-      myInput.focus()
-    })
+  
     </script>
     @endpush
