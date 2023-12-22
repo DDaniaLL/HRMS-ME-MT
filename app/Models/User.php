75d5f7d93bf594713d8e819d6f44533bad98e622
DDaniall <dany.janboura@gmail.com>
2023-12-22 12:39:09 +0300

    copied user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,40 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\CausesActivity;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use CausesActivity;
+    use SoftDeletes;
+    use LogsActivity;
+
+
+    protected static $recordEvents = ['created', 'updated'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name',
+                'birth_date',
+                'unit',
+                'position',
+                'employee_number',
+                'joined_date',
+                'grade',
+                'department',
+                'email',
+                'linemanager',
+                'usertype_id',
+                'hradmin'])->logOnlyDirty();
+
+        // Chain fluent methods for configuration options
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -19,8 +48,20 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'birth_date',
+        'department',
+        'grade',
+        'position',
+        'employee_number',
+        'contract',
+        'joined_date',
         'email',
+        'office',
         'password',
+        'linemanager',
+        'usertype_id',
+        'hradmin',
+        'token',
     ];
 
     /**
@@ -42,4 +83,31 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function leaves()
+    {
+        return $this->hasMany(Leave::class);
+    }
+
+    public function overtimes()
+    {
+        return $this->hasMany(Overtime::class);
+    }
+
+    public function balances()
+    {
+        return $this->hasMany(Balance::class);
+    }
+
+    public function Usertype()
+    {
+        return $this->belongsTo(Usertype::class);
+    }
+
+    public function comlists()
+    {
+        return $this->hasMany(Comlist::class);
+    }
+
+    
 }
